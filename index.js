7e121009ba80eaa35e84fc49d8bc942c9b2f8b4d
Maria Popascu <marrie.popescu@gmail.com>
2024-05-11 12:40:14 +0200

    tema 1, sprint7

---
+++ index.js
@@ -0,0 +1,12 @@
+// "Tipăriți toate numerele pare de la 0 la 10"
+// "2, 4, 6, 8, 10"
+
+function display_smaller_numbers_in_ascending_order (n) {
+    for (let i=2; i<=n; i+=2) {
+        console.log (i);
+    }
+}
+
+
+display_smaller_numbers_in_ascending_order (10)
+
