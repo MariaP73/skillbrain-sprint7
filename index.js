068c68a38c04aa61a1c6c9b239b7776ab1867983
Maria Popascu <marrie.popescu@gmail.com>
2024-05-12 16:23:40 +0200

    ex6

---
+++ index.js
@@ -0,0 +1,10 @@
+// Creați o funcție care inversează un șir de numere
+// "1, -5, 20, -34, 16, 29, 36, -4"
+
+
+let numere = ["1", "-5", "20", "-34", "16", "29", "36", "-4"];
+
+
+const numereReversed = numere.reverse();
+
+console.log(numereReversed);
